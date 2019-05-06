36137eee9df94b47f5eab48bd2d48a11d03d69c6
Claudia Bodea <bodea.claudia.maria@gmail.com>
2019-05-06 13:13:35 +0300

    First commit

---
+++ footer.php
@@ -0,0 +1,31 @@
+
+<footer>
+<div class="footer">
+	<div id="title">	
+      	<h1>Palo Alto</h1>
+    </div>
+	<div class="container">
+		<ul>
+			<li><a href="index.html">Home</a></li>
+    				<li><a href="about.html">About</a></li>
+    				<li><a href="archive.html">Archive</a></li>
+    				<li><a href="contact.html">Contact</a></li>
+    				<li><a href="search.html">⌕</a></li>
+		</ul>
+	</div>
+	<div class ="copyrights">
+		<small>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</small><br>
+		<small>&copy; 2015 Palo Alto. All Rights Reserved. Designed & Developed by PixelBudha Team </small>
+	</div>
+	<div class="follow">
+		<a href="http://facebook.com"><i class="fab fa-facebook-f"></i></a>
+		<a href="http://twitter.com"><i class="fab fa-twitter"></i></a>
+		<a href="http://instagram.com"><i class="fab fa-instagram"></i></a>
+		<a href="http://pinterest.com"><i class="fab fa-pinterest"></i></a>
+	</div>	
+</div>
+
+<?php wp_footer(); ?>       
+</footer>
+</body>
+</html>
