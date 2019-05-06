590646aab69aa6ea15716c5d17d62730f03ed627
Claudia Bodea <bodea.claudia.maria@gmail.com>
2019-05-06 23:46:15 +0300

    Header 1.0

--- header.php
+++ header.php
@@ -1,37 +1,38 @@
 <?php
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
-<head>
-		<meta charset="UTF-8">
-		<title>✿ IAP clautheme</title>  		
-        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">     
 
-		<?php wp_head(); ?>
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-		<link href="<?php echo get_template_directory_uri(); ?>/reset.css" rel="stylesheet">		
-		<link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet">
-		<link href="<?php echo get_template_directory_uri(); ?>/css/header.css" rel="stylesheet">
-		<link href="<?php echo get_template_directory_uri(); ?>/css/footer.css" rel="stylesheet">
-</head>
+  <head>
+    <meta charset="UTF-8">
+    <title>✿ IAP clautheme</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,700i" rel="stylesheet">
 
-<body>
-  	<header>
-  		<div class="container">
-			<div class="header">
-				<div id="title">	
-      				<h1>Palo Alto</h1>
-      			</div>
-      		<nav>
-      			<ul>
-    				<li><a href="index.html">Home</a></li>
-    				<li><a href="about.html">About</a></li>
-    				<li><a href="archive.html">Archive</a></li>
-    				<li><a href="contact.html">Contact</a></li>
-    				<li><a href="search.html">⌕</a></li>
-    			</ul>
-  	 		 <nav>
-  			</div>
-  		</div>
+    <?php wp_head(); ?>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link href="<?php echo get_template_directory_uri(); ?>/reset.css" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri(); ?>/css/header.css" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri(); ?>/css/footer.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <header>
+      <div class="container">
+        <div class="header">
+          <div id="title">
+            <h1>Palo Alto</h1>
+          </div>
+          <nav>
+            <ul>
+              <li><a href="index.html">HOME</a></li>
+              <li><a href="about.html">ABOUT</a></li>
+              <li><a href="archive.html">ARCHIVE</a></li>
+              <li><a href="contact.html">CONTACT</a></li>
+              <li><a href="search.html"><img src="images/search.png" alt = "⌕"></a></li>
+            </ul>
+            <nav>
+        </div>
+      </div>
     </header>
-      
